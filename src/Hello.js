b967e7a62a6e6c89217492768ef721287508be55
Ahsan Jamal <ahsanjamal852@gmail.com>
2023-09-15 14:18:51 -0700

    app interactive with user using the data by map function

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React, {component} from 'react';
+
+class Hello extends component{
+   render() {
+       return(
+           <h1>Hello World</h1>
+       );
+    }
+}
+export default Hello
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+//import App from './App';
+import registerservicesworker from './registerservicesworker';
+
+ReactDoM.render(<H1>Hello World<H1>,docoment.getElementById('root'))
+registerservicesworker();
